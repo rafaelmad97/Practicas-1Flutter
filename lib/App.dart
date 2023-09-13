d9be53f67be26857eccb775113d9dd121ea6b039
Rafael <roboticra@gmail.com>
2023-09-12 20:02:53 -0500

    primera pagina para el desarrollo de la practica para la app ios

---
+++ lib/App.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:modulo1_contador_personalizado/screens/Homepage.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+        title: "iosDEV",
+        theme: CupertinoThemeData(primaryColor: Colors.red),
+        home: HomeScreen());
+  }
+}
