3202b2dcd84789c123b548ede8e172cbc7b24d63
Rafael <roboticra@gmail.com>
2023-09-12 22:37:02 -0500

    se creo la appbar al mejor estilo ios

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("HomeScreen"),
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Contador"),
+        trailing: CupertinoButton(child: const Text("Reset"), onPressed: null),
       ),
-      body: Center(child: Text("Texto")),
+      child: Center(child: Text("wii")),
     );
   }
 }
