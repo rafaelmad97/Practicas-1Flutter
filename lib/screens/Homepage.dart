6711f41bd8c1f6398fca021c6d651e34903a153a
Rafael <roboticra@gmail.com>
2023-09-12 21:06:42 -0500

    se implementó el primer boton flotante

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -3,10 +3,17 @@ import 'package:flutter/material.dart';
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
+  void onPressFloatingbutton() {
+    print("hola aqui hago una vaina");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Android")),
-    );
+    return Scaffold(
+        body: const Center(child: Text("Android")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: onPressFloatingbutton,
+          child: const Icon(Icons.add),
+        ));
   }
 }
