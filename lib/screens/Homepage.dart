e0f5c8e7b95e75e9f33e7be2d6b2709cee970a41
Rafael <roboticra@gmail.com>
2023-09-12 21:41:37 -0500

    se incluyó el manejo de estado

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _counter = 0;
 
   void onPressFloatingbutton() {
     print("hola aqui hago una vaina");
